e80f491fe4ee57faebfa4a9bac6ec95cd1e02441
Eliel P. Ferreira <130425050+eliel-pf-99@users.noreply.github.com>
2024-10-23 11:28:46 -0300

    Update projects.js

--- sessions/projects/projects.js
+++ sessions/projects/projects.js
@@ -1,4 +1,4 @@
-export const Projects = `<div class="projects sections">
+const Projects = `<div class="projects sections">
     <h1 class="title-projects">Projects</h1>
     <div class="elements">
       <div class="project-elem">
