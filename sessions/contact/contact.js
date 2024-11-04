a1f621aef0842bc86c68f24c839de36bb6d199f7
Eliel P. F <eliel.dev.99@gmail.com>
2024-11-04 00:59:40 -0300

    yeah we are more near

--- sessions/contact/contact.js
+++ sessions/contact/contact.js
@@ -1,19 +1,19 @@
 const Contact = language => `<div id="contact" class="contact sections">
     <h1 class="title-contact">${language.contact.title}</h1>
     <div class="me">
-      <img src="https://ee76vrazytu.exactdn.com/wp-content/uploads/2024/03/abcdhe-201.jpg?strip=all&lossy=1&ssl=1" alt="">
+      <div class="img"></div>
       <div class="me-items">
         <div class="me-item">
           <i class="fa-brands fa-instagram"></i>
-          <a target="_blank" href="https://www.instagram.com/eliel_p_f/profilecard/?igsh=dTBydnh6bWduOGk=">@eliel_p_f</a>
+          <a target="_blank" href="https://www.instagram.com/eliel_p_f/profilecard/?igsh=dTBydnh6bWduOGk=">eliel_p_f</a>
         </div>
         <div class="me-item">
           <i class="fa-brands fa-linkedin"></i>
-          <a href="#">@eliel_p_f</a>
+          <a href="https://linkedin.com/in/elielpereiraf99">elielpereiraf99</a>
         </div>
         <div class="me-item">
           <i style="font-size: 42px;" class="fa-solid fa-at"></i>
-          <a href="mailto:eliel.dev.99@gmail.com">@eliel_p_f</a>
+          <a href="mailto:eliel.dev.99@gmail.com">mail</a>
         </div>
       </div>
       
